e4c5f142bd83526cdbf895b496939154180d87ee
mysantos <mylenaspmendes@gmail.com>
2019-07-11 10:39:31 -0300

    exercicio if e else

---
+++ script.js
@@ -0,0 +1,20 @@
+// // Sistema que avalia se o aluno foi aprovado ou não em uma matéria. Pergunte para o usuario:  O nome do aluno, a matéria, a nota1. Com os valores recebidos,verifique se o aluno teve  nota superior a média (6). A saida do programa deve falar  "Parabéns, o aluno X foi aprovado na materia Y" OU "Parece que a nota do aluno X na meteria Y foi insuficiente" 
+
+// let nome = (prompt("Digite seu nome?"));
+// let materia = (prompt("Qual a materia?"));
+// let nota = Number(prompt("Digite sua nota:"));
+
+// let media = 6;
+
+// if(Number(nota) >= media) {
+//     alert(`Parabéns, o aluno ${nome} foi APROVADO na materia ${materia}`); 
+// }
+
+// else if(Number (nota) >= media - 0.5){
+//     alert(`Parabéns, o  ${nome} foi APROVADO na materia ${materia} pelo sistema`); 
+// }
+// else{
+//     // alert(`Com a nota ${nota} você foi Reprovado`);
+// }
+
+
